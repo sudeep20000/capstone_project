50fbf8a3bc80c253594471f21193bf5d438e31ad
sudeep500 <sudipta13.chakraborty@gmail.com>
2023-10-06 13:49:01 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
-import './index.css';
 import Signinpage from './pages/signinpage/Signinpage';
 import Categorypage from './pages/categorypage/Categorypage';
+import './index.css';
 
 export default function App() {
   return (
